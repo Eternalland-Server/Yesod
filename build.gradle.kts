5fbe8264a711330a132b31837812d9acb89ef6a5
kirraObj <c@skymc.me>
2022-11-24 00:34:14 +0800

    update.

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    java
+    id("io.izzel.taboolib") version "1.31"
+    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+}
+
+taboolib {
+    description {
+        dependencies {
+            name("Multiverse-Core").optional(true)
+        }
+        load("STARTUP")
+        contributors {
+            name("坏黑")
+            name("闲蛋")
+        }
+    }
+    install("common")
+    install("common-5")
+    install("module-configuration")
+    install("module-chat")
+    install("module-nms", "module-nms-util")
+    install("platform-bukkit")
+    install("expansion-command-helper")
+    classifier = null
+    version = "6.0.7-64"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("net.sakuragame.eternal:JustMessage:1.0.4-SNAPSHOT@jar")
+    compileOnly("org.reflections:reflections:0.10.2")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    compileOnly("net.sakuragame:DataManager-Bukkit-API:1.3.2-SNAPSHOT@jar")
+    compileOnly("ink.ptms.core:v11200:11200")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if (requested.group == "org.jetbrains.kotlin") {
+                useVersion("1.5.10")
+            }
+        }
+    }
+}
